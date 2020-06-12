86ceb92cdbf54d4b040ad64ee73b6d558050f5f9
E-Cviceni <66831189+E-Cviceni@users.noreply.github.com>
2020-06-12 14:32:34 +0200

    přidán html kod

---
+++ e-cviceni.cf/htdocs/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>E-Cvičení</title>
+		<link type="text/css" rel="stylesheet" href="styles.css"/>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+	  <header>
+	    <h1>E-Cvičení</h1>
+	    <nav>
+        <ul>
+            <li><a href="index.html">Domů</a></li>
+            <li><a href="cviceni.html">Cvičení</a></li>
+			<li><a href="onas.html">O nás</a></li>
+			<li><a href="kontakt.html">Kontakt</a></li>
+            <li><a href="http://e-zapisky.cf">E-Zápisky</a></li>
+        </ul>
+    </nav>
+	  </header>
+	  
+	  <main>
+	    <h2>Vítejte na stránce E-Cvičení!</h2>
+	    
+	    <p>
+	      E-Cvičení je docela malá webová stránka na procvičován českého jazyku, matematika, a podobných předmětů.
+	    </p>
+		<p>
+		  Perfektní pomůcka pro učitele a žaký kteří si chtějí procvičit nějaký z předmětů.
+		</p>
+		<h2>Doporučené</h2>
+		<div class="ContainBox">
+		    <div class="ActivBox">
+			    <h3>Slovanské Jazyky</h3>
+				<p>Český Jazyk</p>
+			</div>
+			<div class="ActivBox">
+			    <h3><a href="http://e-cviceni.cf/cviceni/8rocnik/matematika/pi.html">Pí</a></h3>
+				<p>Matematika</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Elektrický Náboj</h3>
+				<p>Fyzika</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Barokní Hudba</h3>
+				<p>Hudebka</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Jan Neruda</h3>
+				<p>Literatura</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Velká Písmena</h3>
+				<p>Český Jazyk</p>
+			</div>
+		    <div class="ActivBox">
+			    <h3><a href="http://e-cviceni.cf/cviceni/8rocnik/matematika/zapis-rovnice.html">Zápis rovnice</a></h3>
+				<p>Matematika</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Doprava A Spoje</h3>
+				<p>Zeměpis</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>1. Sv. Válka</h3>
+				<p>Dějepis</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Podmínkové Věty</h3>
+				<p>Angličtina</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Vitamíny</h3>
+				<p>Přírodopis</p>
+			</div>
+			<div class="ActivBox">
+			    <h3>Sektor Služeb</h3>
+				<p>Zeměpis</p>
+			</div>
+		</div>
+	  </main>
+	  <footer>
+	  <hr>
+	    <p>© Daniel Coats 2020</p>
+        <?php
+            echo "<h2>PHP is Fun!</h2>";
+            echo "Hello world!<br>";
+            echo "I'm about to learn PHP!<br>";
+            echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+        ?>
+	  </footer>
+	</body>
+</html>
